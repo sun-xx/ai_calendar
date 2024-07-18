a0889b758f72ddb7ef0fc0bf614c67350877ff89
Nash Sun <nash_sun@outlook.com>
2024-07-18 17:07:34 +0800

    AI utils update.

---
+++ utils/bot.py
@@ -0,0 +1,35 @@
+import erniebot
+import os
+import re
+import datetime
+
+def extract_info(input_str): 
+    erniebot.api_type = 'aistudio'
+    erniebot.access_token = os.environ.get('ERNIE_ACCESS_TOKEN')
+    # Set your own access_token of baidu in environment variable.
+
+    dt = datetime.datetime.today()
+    dt = dt.strftime('%Y-%m-%d')
+
+    model = 'ernie-4.0'
+    prompt = "请从以上语句中提取出日期（转换为YYYY-mm-dd格式）、时间（几点几分）、事件、地点，将以上要素按顺序输出，每两个之间空一格。如果时间表述不清，则选择合理的时间，如下午替换为2pm，上午替换为7am。如果有要素缺失，请用暂无替代。"
+    prompt2 = "注意今天是" + dt + "。"
+    prompt3 = "输出以上要素时，请用英文括号将四个要素整体括起来，例如：(2024-7-19 14:00 写作 家)"
+
+    messages = [{'role': 'user', 'content': input_str + prompt + prompt2 + prompt3}]
+    def main(model, messages):
+        response = erniebot.ChatCompletion.create(
+            model = model,
+            messages = messages,
+            top_p = 0.01)
+        result = response.get_result()
+        return result
+
+    answer = main(model, messages)
+    match = re.search(r'\((.*?)\)', answer)
+    if match:
+        return match.group(1)
+    else:
+        return None
+
+#print(extract_info("后天晚上六点到原神游泳馆游泳"))
---
+++ utils/ocr.py
@@ -0,0 +1,59 @@
+import os
+import requests
+from urllib.request import urlopen
+from urllib.request import Request
+from urllib.error import URLError
+from urllib.parse import urlencode
+import time
+import json
+timer = time.perf_counter
+import base64
+
+def fetch_token():
+    API_KEY = os.environ.get("API_KEY")
+    SECRET_KEY = os.environ.get("SECRET_KEY")
+    TOKEN_URL = 'http://aip.baidubce.com/oauth/2.0/token'
+    params = {'grant_type': 'client_credentials',
+              'client_id': API_KEY,
+              'client_secret': SECRET_KEY}
+    post_data = urlencode(params)
+    post_data = post_data.encode( 'utf-8')
+    req = Request(TOKEN_URL, post_data)
+    try:
+        f = urlopen(req)
+        result_str = f.read()
+    except URLError as err:
+        print('token http response http code : ' + str(err.code))
+        result_str = err.read()
+    
+    result_str =  result_str.decode()
+
+    result = json.loads(result_str)
+    if 'access_token' in result.keys(): 
+        return result['access_token']
+    else:
+        raise None
+
+def ocr(image_path):
+    request_url = "https://aip.baidubce.com/rest/2.0/ocr/v1/accurate_basic"
+    f = open(image_path, 'rb')
+    img = base64.b64encode(f.read())
+
+    params = {"image":img}
+    access_token = os.environ.get("ACCESS_TOKEN") # Set your own access token in your environment variables.
+    request_url = request_url + "?access_token=" + fetch_token()
+    headers = {'content-type': 'application/x-www-form-urlencoded'}
+    response = requests.post(request_url, data=params, headers=headers)
+    if response:
+        data = response.json()
+        data = data["words_result"]
+        s = ''
+        for i in data:
+            s += i["words"]
+
+        return s
+    else:
+        return None
+    
+print(ocr("c:/users/pc/desktop/test.jpg"))
+

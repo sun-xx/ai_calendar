c575750413174b50a5a6ad77ee993b521dcbf8f9
Nash Sun <nash_sun@outlook.com>
2024-07-19 08:41:55 +0800

    add select function in GUI.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
 import PyQt5
+import PyQt5.Qt
 import PyQt5.QtCore
 import PyQt5.QtGui
 from PyQt5.QtWidgets import *
@@ -7,15 +8,23 @@ import CDSL.db as db
 from utils import ocr, bot
 
 class Thread(PyQt5.QtCore.QThread):
-    def __init__(self, msg, parent=None):
+    def __init__(self, msg, signal, parent=None):
         super(Thread, self).__init__(parent)
         self.msg = msg
+        self.sig = signal
     
     def run(self):
-        text = bot.extract_info(self.msg)
-        info = text.split()
-        print(info)
-        db.insert_data(date_str=info[0], hour_str=info[1], event_str=info[2], location_str=info[3])
+        if self.sig == 'single':
+            text = bot.extract_info(self.msg)
+            info = text.split()
+            print(info)
+            db.insert_data(date_str=info[0], hour_str=info[1], event_str=info[2], location_str=info[3])
+        if self.sig == 'multi':
+            for t in self.msg:
+                text = bot.extract_info(t)
+                info = text.split()
+                print(info)
+                db.insert_data(date_str=info[0], hour_str=info[1], event_str=info[2], location_str=info[3])
 
 
 class CalendarDialog(QDialog):
@@ -87,8 +96,10 @@ class MainWindow(QWidget):
         self.right_layout.addWidget(self.calendar)
 
     def show_information(self, date):
+        global ch
         date = date.toString("yyyy-MM-dd")
         print(date)
+        ch.show(db.select_data(date))
 
     def show_dialog(self):
         if self.status == 'dialog':
@@ -143,7 +154,10 @@ class MainWindow(QWidget):
         image_file = QFileDialog.getOpenFileName(self, 'Open file', 'C:/', 'Image files (*.jpg *.png *.jpeg)')
         if image_file[0]:
             text = ocr.ocr(image_file[0])
-            self.work_thread = Thread(text)
+            if type(text).__name__ == 'list':
+                self.work_thread = Thread(msg=text, signal='multi')
+            if type(text).__name__ == 'str':
+                self.work_thread = Thread(msg=text, signal='single')
             self.work_thread.start()
         else:
             print('No file selected')
@@ -154,8 +168,27 @@ class MainWindow(QWidget):
         self.lineedit.clear()
         self.work_thread.start()
 
+
+class ChildWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle('今日日程')
+        self.resize(400, 300)
+    
+    def show(self, info):
+        self.browser = QTextBrowser(self)
+        self.browser.setFont(PyQt5.QtGui.QFont('微软雅黑', 14))
+        self.browser.setFixedHeight(300)
+        self.browser.setFixedWidth(400)
+        for i in info:
+            st = i[0] + i[1] + ',在' + i[2] + '\n'
+            self.browser.append(st)
+        super().show()
+
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = MainWindow()
+    ch = ChildWindow()
     ex.show()
     sys.exit(app.exec_())
